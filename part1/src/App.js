763e10b261db6b68005e3c0351b05011247085fa
Hla Htun <kshap.karki@gmail.com>
2023-04-16 00:38:47 -0700

    worked on exercise 1.6

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,48 +1,27 @@
+import { useState } from "react";
+
 const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-            {
-                name: 'Fundamentals of React',
-                exercises: 10
-            },
-            {
-                name: 'Using props to pass data',
-                exercises: 7
-            },
-            {
-                name: 'State of a component',
-                exercises: 14
-            }
-        ]
-    }
-    const Header = (props) => {
-        return <h1>{props.name}</h1>
-    }
+    // save clicks of each button to its own state
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
 
-    const Content = (props) => {
-        return (
-            <div>
-                <Part part={props.parts[0]} />
-                <Part part={props.parts[1]} />
-                <Part part={props.parts[2]} />
-            </div>
-        )
-    }
-
-    const Part = (props) => {
-        return <p>{props.part.name} {props.part.exercises}</p>
-    }
-
-    const Total = (props) => {
-        return <p>Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>
-    }
+    const handleClick = (setValue, value) => setValue(value+1)
+    const Button = ( {setValue, value, text}) => <button onClick={() => handleClick(setValue, value)}>{text}</button>
+    const Stats = ( {text, value}) => <div>{text} {value}</div>
 
     return (
         <div>
-            <Header name={course.name} />
-            <Content parts={course.parts}/>
-            <Total parts={course.parts}/>
+            <h1>give feedback</h1>
+            <Button setValue={setGood} value={good} text={"good"} />
+            <Button setValue={setNeutral} value={neutral} text={"neutral"} />
+            <Button setValue={setBad} value={bad} text={"bad"} />
+
+            <h1>statistics</h1>
+            <Stats text={"good"} value={good} />
+            <Stats text={"neutral"} value={neutral} />
+            <Stats text={"bad"} value={bad} />
+
         </div>
     )
 }
