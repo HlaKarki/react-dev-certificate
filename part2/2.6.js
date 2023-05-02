4d34834cdc0ae7768911563a01695f8e1b06a0c3
Hla Htun <kshap.karki@gmail.com>
2023-05-02 09:21:15 -0700

    submission for part 2.6

---
+++ part2/2.6.js
@@ -0,0 +1,39 @@
+import { useState } from 'react'
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        {name: 'Arto Hellas'}
+    ])
+    const [newName, setNewName] = useState('')
+
+    const addNewName = (event) => {
+        event.preventDefault()
+        const newObject = {
+            name: newName
+        }
+        setPersons(persons.concat(newObject))
+        setNewName('')
+    }
+
+    const handleChange = (event) => {
+        setNewName(event.target.value)
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onClick={addNewName}>
+                <div>
+                    name: <input value={newName} onChange={handleChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            { persons.map(person => <div key={person.name}>{person.name}</div>) }
+        </div>
+    )
+}
+
+export default App
