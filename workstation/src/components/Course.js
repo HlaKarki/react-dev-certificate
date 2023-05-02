2af967d42bde9e9e4d0713ad21161e25f7b13e5b
Hla Htun <kshap.karki@gmail.com>
2023-05-02 08:18:38 -0700

    working on the solution for 2.5

--- workstation/src/components/Course.js
+++ workstation/src/components/Course.js
@@ -1,3 +1,43 @@
+const courses = [
+    {
+        id: 1,
+        name: 'Half Stack application development',
+        parts: [
+            {
+                name: 'Fundamentals of React',
+                exercises: 10,
+                id: 1
+            },
+            {
+                name: 'Using props to pass data',
+                exercises: 7,
+                id: 2
+            },
+            {
+                name: 'State of a component',
+                exercises: 14,
+                id: 3
+            }
+        ]
+    },
+    {
+        name: 'Node.js',
+        id: 2,
+        parts: [
+            {
+                name: 'Routing',
+                exercises: 3,
+                id: 1
+            },
+            {
+                name: 'Middlewares',
+                exercises: 7,
+                id: 2
+            }
+        ]
+    }
+]
+
 const Header = ({ name }) => <h1>{name}</h1>
 
 const Content = ({ parts }) => {
@@ -29,7 +69,7 @@ const Course = ({ course }) => {
     )
 }
 
-const Courses = ({courses}) => {
+const Courses = () => {
     return (
         <div>
             {courses.map(course =>
