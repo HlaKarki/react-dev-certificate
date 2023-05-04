1fae9c21fc22385db751da5c9c277d4e37cc0442
Hla Htun <kshap.karki@gmail.com>
2023-05-04 09:28:40 -0700

    solution for part 2.9

--- workstation/src/App.js
+++ workstation/src/App.js
@@ -2,13 +2,14 @@ import { useState } from 'react'
 
 const App = () => {
     const [persons, setPersons] = useState([
-        {
-            name: 'Arto Hellas',
-            number: '040-1234567'
-        }
+        { name: 'Arto Hellas', number: '040-123456', id: 1 },
+        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
     ])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
+    const [newFilter, setNewFilter] = useState('')
 
     const addNewName = (event) => {
         event.preventDefault()
@@ -30,9 +31,36 @@ const App = () => {
         setNewNumber(event.target.value)
     }
 
+    const filterName = (event) => {
+        event.preventDefault()
+        if (event.target.value === '') {
+            setPersons([
+                { name: 'Arto Hellas', number: '040-123456', id: 1 },
+                { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+                { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+                { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+            ]);
+        } else {
+            const filteredPersons = persons.filter( person =>
+                person.name.toLowerCase().includes(event.target.value.toLowerCase())
+            )
+            setPersons(filteredPersons)
+        }
+
+    }
+
+    const newFilterChange = (event) => {
+        setNewFilter(event.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
+            <form onChange={filterName}>
+                filter shown with <input value={newFilter} onChange={newFilterChange}/>
+            </form>
+
+            <h2>add a new</h2>
             <form onClick={addNewName}>
                 <div>
                     name: <input value={newName} onChange={newNameChange}/>
