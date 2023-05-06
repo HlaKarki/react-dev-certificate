ff4e7a415f696411e368f4a7aa5f1783d6b0ab48
Hla Htun <kshap.karki@gmail.com>
2023-05-05 23:17:36 -0700

    solution for part 2.10

---
+++ workstation/src/components/part2/PersonsFiltered.js
@@ -0,0 +1,9 @@
+const PersonsFiltered = ({newPersons}) => {
+    return (
+        <div>
+            { newPersons.map(person => <div key={person.name}>{person.name} {person.number}</div>) }
+        </div>
+    )
+}
+
+export default PersonsFiltered
