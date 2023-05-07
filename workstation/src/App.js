b55751bc5f1f5f41a462f97ec0983a7492d0bfc9
Hla Htun <kshap.karki@gmail.com>
2023-05-06 18:43:02 -0700

    solution for part 2.11

--- workstation/src/App.js
+++ workstation/src/App.js
@@ -1,20 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { default as Filter } from './components/part2/Filter'
 import { default as PersonForm } from './components/part2/PersonForm'
 import { default as Persons } from './components/part2/Persons'
 import { default as PersonsFiltered } from './components/part2/PersonsFiltered'
+import axios from "axios";
 
 const App = () => {
-    const [persons, setPersons] = useState([
-        { name: 'Arto Hellas', number: '040-123456', id: 1 },
-        { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-        { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-        { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-    ])
+    const [persons, setPersons] = useState([])
     const [newName, setNewName] = useState('')
     const [newNumber, setNewNumber] = useState('')
     const [newFilter, setNewFilter] = useState('')
     const [newPersons, setNewPersons] = useState([])
+
+    const hook = () => {
+        console.log('effect');
+        axios
+            .get('http://localhost:3001/persons')
+            .then(response => {
+                console.log('promise fulfilled');
+                setPersons(response.data)
+            })
+    }
+    useEffect(hook, [])
 
     const addNewName = (event) => {
         event.preventDefault()
