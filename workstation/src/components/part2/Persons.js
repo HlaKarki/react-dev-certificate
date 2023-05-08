c54c902e581fc6d727f9ff28084eb7080c69e4b2
Hla Htun <kshap.karki@gmail.com>
2023-05-07 21:23:04 -0700

    solution for part 2(b).14

--- workstation/src/components/part2/Persons.js
+++ workstation/src/components/part2/Persons.js
@@ -1,7 +1,9 @@
-const Persons = ({persons}) => {
+import { default as DeletePerson } from './DeletePerson'
+
+const Persons = ({persons, deletePerson}) => {
     return (
         <div>
-            { persons.map(person => <div key={person.name}>{person.name} {person.number}</div>) }
+            { persons.map(person => <div key={person.name}>{person.name} {person.number} <DeletePerson deletePerson={deletePerson} name={person.name}/></div>) }
         </div>
     )
 }
