78cf5b1cf96642d4cf65718741712c3c8d6a36b9
Hla Htun <kshap.karki@gmail.com>
2023-05-07 21:49:03 -0700

    solution for part 2(b).15

--- workstation/src/services/phonebook.js
+++ workstation/src/services/phonebook.js
@@ -16,5 +16,9 @@ const deleteP = (id) => {
     return request.then(() => null)
 }
 
+const updateContact = (id, newContact) => {
+    const request = axios.put(`${baseUrl}/${id}`, newContact)
+    return request.then(response => response.data)
+}
 // eslint-disable-next-line
-export default { getAll, create, deleteP }
+export default { getAll, create, deleteP, updateContact }
